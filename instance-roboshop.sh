91b68fbfaff6bcf729826f763415a8d7f7f25f78
yash8575 <allamyash0609@gmail.com>
2026-06-04 15:41:31 +0200

    roboshop

--- instance-roboshop.sh
+++ instance-roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 Sg_ID="sg-03ca0ca07c63fb908"
+zone_ID="Z04331122AEWGW4IMJX9W"
+DOMAIN_NAME="ramyaboutique.shop"
 
 for instance in $@
 do
@@ -22,6 +24,8 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
+        RECORD_NAME="$DOMAIN_NAME"
+
     else
           IP=$(
             aws ec2 describe-instances \
@@ -29,6 +33,28 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
         echo "Ip Address $IP"
+
+    aws route53 change-resource-record-sets --hosted-zone-id $zone_ID --change-batch '{
+  "Comment": "Updating the A record for the main website",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "$RECORD_NAME.",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          {
+            "Value": "$IP"
+          }
+        ]
+      }
+    }
+  ]
+}
+'
+
 done
