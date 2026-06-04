1115f429e34b23db8f676b53ba083dd950d533e4
yash8575 <allamyash0609@gmail.com>
2026-06-04 15:14:26 +0200

    roboshop

--- instance-roboshop.sh
+++ instance-roboshop.sh
@@ -5,11 +5,30 @@ Sg_ID="sg-03ca0ca07c63fb908"
 
 for instance in $@
 do
-    aws ec2 run-instances \
+    INSTANCE_ID = $( aws ec2 run-instances \
   --image-id $AMI_ID \
   --instance-type t3.micro \
   --security-group-ids $Sg_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-  --query 'Instances[0].PrivateIpAddress' \
+  --query 'Instances[0].InstanceId' \
   --output text
+    )
+
+    if [ $instance = "frontend" ]; then
+
+         IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+    else
+          IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+    fi
+        echo "Ip Address $IP"
 done
