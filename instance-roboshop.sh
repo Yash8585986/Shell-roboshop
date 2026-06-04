dbb5e4b131ef293ac3d1e97b34fe45011be8dfd4
yash8575 <allamyash0609@gmail.com>
2026-06-04 15:58:42 +0200

    roboshop

--- instance-roboshop.sh
+++ instance-roboshop.sh
@@ -43,12 +43,12 @@ do
     {
       "Action": "UPSERT",
       "ResourceRecordSet": {
-        "Name": "$RECORD_NAME",
+        "Name": "'$RECORD_NAME'",
         "Type": "A",
-        "TTL": 300,
+        "TTL": 1,
         "ResourceRecords": [
           {
-            "Value": "$IP"
+            "Value": "'$IP'"
           }
         ]
       }
