dd50229bd93264d5f4578d1227d161e63ae59cc2
yash8575 <allamyash0609@gmail.com>
2026-06-05 19:44:37 +0200

    roboshop

--- user.sh
+++ user.sh
@@ -64,7 +64,7 @@ validate $? "user unzip"
 npm install &>> $LOG_FILE
 validate $? "user npm install"
 
-cp /$SCRIPT_DIR/user.service /etc/systemd/system/user.service &>> $LOG_FILE
+cp user.service /etc/systemd/system/user.service &>> $LOG_FILE
 validate $? "user service file copy"
 
 systemctl daemon-reload &>> $LOG_FILE
