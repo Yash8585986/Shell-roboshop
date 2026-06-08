4fc052c3f87331989bd4a130dabb146ebf11fcc5
yash8575 <allamyash0609@gmail.com>
2026-06-08 17:41:30 +0200

    roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-scripts"
+LOG_FILE="$LOG_FOLDER/$0.sh"
+SCRIPT_DIR="$PWD"
 
 if [ $USERID -ne 0]; then
     echo "Please run the script with root or sudo user "
     exit 1
 fi
 
-dnf install rabbitmq-server -y
+validate (){
+    if [ $1 -ne 0 ]; then
+        echo "$2 installation is failed"
+    else
+        echo "$2 installation is success"
+    fi
+}
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+cp /SCRIPT_DIR/rabbitmq.rep /etc/yum.repos.d/rabbitmq.repo
+validate $? "added rabbitmq repo"
+
+dnf install rabbitmq-server -y &>> $LOG_FILE
+validate $? "rabbitmq-server"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+systemctl enable rabbitmq-server &>> $LOG_FILE
+validate $? "Enable rabbitmq"
+
+systemctl start rabbitmq-server &>> $LOG_FILE
+validate $? "Starting rabbitmq"
+
