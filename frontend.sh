bcae6c1aa013bbcace1c23debc04bc319d859e5c
yash8575 <allamyash0609@gmail.com>
2026-06-08 19:10:29 +0200

    roboshop

--- frontend.sh
+++ frontend.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-LOG_FOLDER="/var/log/shell-roboshop"
-LOG_FILE="$LOG_FOLDER/$0.log"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws88s.online
 
-
-if [ $? -ne 0 ]; then
-    echo "Please try with root user"
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
 
-validate(){
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 Intallation failed"
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
     else
-        echo "$2 Installation successfull"
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+dnf module disable nginx -y &>>$LOGS_FILE
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx  &>>$LOGS_FILE
+systemctl start nginx 
+VALIDATE $? "Enabled and started nginx"
 
 rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Remove default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOGS_FILE
+VALIDATE $? "Downloaded and unzipped frontend"
 
 rm -rf /etc/nginx/nginx.conf
 
-cp /$SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-validate $? "Copied our nginx conf file"
-
-
-
-systemctl enable nginx 
-systemctl start nginx 
-
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copied our nginx conf file"
 
 systemctl restart nginx
-validate $? "Restarted Nginx"
+VALIDATE $? "Restarted Nginx"
